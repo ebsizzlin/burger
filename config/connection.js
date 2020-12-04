42136f0b6129ca2e3414d720efd7461a2cf6d012
Ella Thompson <fang98t@gmail.com>
2020-12-03 23:48:32 -0500

    connection.js pseudocoding

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,15 @@
+//require mysql
 var mysql = require("mysql");
 
+//database connection details
+var connection = mysql.createConnection({
+
+})
+
+//make the connection
+connection.connect((err) => {
+    
+})
+
+//export it
+module.exports = connection;
