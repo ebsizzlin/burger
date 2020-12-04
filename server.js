f09d10e2f0ed00fcd1a5434de30da89b108685bc
Ella Thompson <fang98t@gmail.com>
2020-12-04 01:48:45 -0500

    writing out server.js

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+//static
+app.use(express.static("public"));
+
+//json parse
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//import routes, give them to server
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+//start server
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+});
+
