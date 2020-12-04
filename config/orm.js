73ff8ea9d737165ad60079a867d3661e5ea20e10
Ella Thompson <fang98t@gmail.com>
2020-12-03 23:57:30 -0500

    pseudocoding orm

---
+++ config/orm.js
@@ -0,0 +1,11 @@
+//import connection
+var connection = require("../config/connection");
+
+//selectAll data function
+
+//insertOne data function
+
+//updateOne data function
+
+//export orm
+module.exports = orm;
