b4f57ebb37e1bfaa6887f115a8937846ee254219
Ella Thompson <fang98t@gmail.com>
2020-12-03 23:52:21 -0500

    writing out connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,17 @@ var mysql = require("mysql");
 
 //database connection details
 var connection = mysql.createConnection({
-
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Ella98th',
+    database: 'burgers_db'
 })
 
 //make the connection
 connection.connect((err) => {
-    
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
 })
 
 //export it
