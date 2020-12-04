b01f8686a0c728e81a9b3806fd672aaf624af664
Ella Thompson <fang98t@gmail.com>
2020-12-04 00:17:55 -0500

    referencing code from CatsApp activity in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,122 @@
 //import connection
 var connection = require("../config/connection");
 
+//adding in js from CatsApp activity
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+function objToSql(ob) {
+var arr = [];
+
+for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+    if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+    }
+    // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+    // e.g. {sleepy: true} => ["sleepy=true"]
+    arr.push(key + "=" + value);
+    }
+}
+return arr.toString();
+}
+
+
 var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      },
+      create: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+
+
     //selectAll data function
-    selectAll:
+    selectAll: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
 
     //insertOne data function
-    insertOne:
+    insertOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
 
     //updateOne data function
-    updateOne
-
+    updateOne: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
 };
 
 //export orm
