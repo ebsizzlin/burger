10430e963072b78d09532bb2cde6eaf860f07d72
Ella Thompson <fang98t@gmail.com>
2020-12-04 00:35:16 -0500

    writing out burger.js

--- models/burger.js
+++ models/burger.js
@@ -4,10 +4,26 @@ var orm = require("../config/orm.js")
 //burger function for specific burger input
 var burger = {
     //all
+    all: (cb) => {
+        orm.all('burgers', (res) => {
+            cb(res);
+        });
+    },
 
     //create
+    create: (cols, vals, cb) => {
+        orm.create('burgers', cols, vals, (res) => {
+            cb(res);
+        });
+    },
 
-    //updatee
-}
+    //update
+    update: (objColVals, condition, cb) => {
+        orm.update('burgers', objColVals, condition, (cb) => {
+            cb(res);
+        });
+    }
+};
 
 //export burger
+module.exports = burger;
