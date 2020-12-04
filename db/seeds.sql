f0221f856552ffbb55b36d10170e37f875d7077c
Ella Thompson <fang98t@gmail.com>
2020-12-03 23:34:35 -0500

    writing out seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- name and whether ate or not inserts
+INSERT INTO burgers (burger_name, devoured) VALUES ('Slider', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Teriyaki', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
