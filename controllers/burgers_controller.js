8d661458aca39cc2ce30ca76cc8ecd9a1f009d96
Ella Thompson <fang98t@gmail.com>
2020-12-04 00:40:06 -0500

    pseudocoding controllers

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+//require
+var express = require("express");
+var burger = require("../models/burger.js");
+
+//create router
+var router = express.Router();
+
+//get
+
+//post
+
+//put
+
+//export router
+module.exports = router;
