8be7a41c2a0bed0868aef7aa4d5840188cc2280e
Ella Thompson <fang98t@gmail.com>
2020-12-04 23:48:56 -0500

    reordering connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,23 +7,23 @@ var connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-      host: 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  var connection = mysql.createConnection({
+      host: 'localhost',
       port: 3306,
-      user: 'rf17qayvakdt1khn',
-      password: 'grtvjlb46to4yu07',
-      database: 'knp557aeez5e5cld'
-    })
+      user: 'root',
+      password: 'Ella98th',
+      database: 'burgers_db'
+  })
 }
 
+// connection = mysql.createConnection({
+//   host: 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//   port: 3306,
+//   user: 'rf17qayvakdt1khn',
+//   password: 'grtvjlb46to4yu07',
+//   database: 'knp557aeez5e5cld'
+// })
 //if local database connection
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Ella98th',
-    database: 'burgers_db'
-})
 
 //make the connection
 connection.connect(function(err) {
